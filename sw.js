e0a36e217f4b06498b7c91d98a3ce9f6153fdb86
Dandyfun <124087430+Dandyfun1@users.noreply.github.com>
2026-03-15 12:30:24 +0100

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,33 @@
+const CACHE="dashboard-cache"
+
+const assets=[
+
+"/",
+
+"/index.html",
+
+"/style.css",
+
+"/script.js"
+
+]
+
+self.addEventListener("install",e=>{
+
+e.waitUntil(
+
+caches.open(CACHE).then(cache=>cache.addAll(assets))
+
+)
+
+})
+
+self.addEventListener("fetch",e=>{
+
+e.respondWith(
+
+caches.match(e.request).then(res=>res||fetch(e.request))
+
+)
+
+})
