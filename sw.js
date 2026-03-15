5a4dbacc8cca0a9ca20ecbe1ad8fafaa80fc3b98
Dandyfun <124087430+Dandyfun1@users.noreply.github.com>
2026-03-15 12:38:54 +0100

    Remove unnecessary line breaks in sw.js

--- sw.js
+++ sw.js
@@ -1,23 +1,16 @@
 const CACHE="dashboard-cache"
 
 const assets=[
-
 "/",
-
 "/index.html",
-
 "/style.css",
-
 "/script.js"
-
 ]
 
 self.addEventListener("install",e=>{
 
 e.waitUntil(
-
 caches.open(CACHE).then(cache=>cache.addAll(assets))
-
 )
 
 })
@@ -25,9 +18,7 @@ caches.open(CACHE).then(cache=>cache.addAll(assets))
 self.addEventListener("fetch",e=>{
 
 e.respondWith(
-
 caches.match(e.request).then(res=>res||fetch(e.request))
-
 )
 
 })
